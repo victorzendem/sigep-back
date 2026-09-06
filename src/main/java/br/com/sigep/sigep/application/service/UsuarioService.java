318ed83921f4d148874d6684a333039d1629b99a
victorzendem <victorzendem@gmail.com>
2026-09-05 23:10:43 -0300

    feat: adiciona criação de usuário com validação implementada

--- src/main/java/br/com/sigep/sigep/application/service/UsuarioService.java
+++ src/main/java/br/com/sigep/sigep/application/service/UsuarioService.java
@@ -1,6 +1,9 @@
 package br.com.sigep.sigep.application.service;
 
 
+import br.com.sigep.sigep.application.dto.usuario.UsuarioRequest;
+import br.com.sigep.sigep.application.dto.usuario.UsuarioResponse;
+import br.com.sigep.sigep.domain.model.Usuario;
 import br.com.sigep.sigep.infraestructure.persistency.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,4 +13,17 @@ import org.springframework.stereotype.Service;
 public class UsuarioService {
 
     private final UsuarioRepository USUARIO_REPOSITORY;
+
+    public UsuarioResponse cadastrar(UsuarioRequest usuarioRequest){
+
+        Usuario usuario = new Usuario(
+                usuarioRequest.nome(),
+                usuarioRequest.email(),
+                usuarioRequest.cpf(),
+                usuarioRequest.senha()
+        );
+        USUARIO_REPOSITORY.save(usuario);
+
+        return UsuarioResponse.from(usuario);
+    }
 }
---
+++ src/main/java/br/com/sigep/sigep/presentation/controller/UsuarioController.java
@@ -0,0 +1,31 @@
+package br.com.sigep.sigep.presentation.controller;
+
+
+import br.com.sigep.sigep.application.dto.usuario.UsuarioRequest;
+import br.com.sigep.sigep.application.dto.usuario.UsuarioResponse;
+import br.com.sigep.sigep.application.service.UsuarioService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/usuario")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+    private final UsuarioService USUARIO_SERVICE;
+
+
+    @PostMapping
+    public ResponseEntity<UsuarioResponse> cadastrar(@Valid @RequestBody UsuarioRequest usuarioRequest){
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(USUARIO_SERVICE.cadastrar(usuarioRequest));
+    }
+
+}
